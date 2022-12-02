4a2a32613babe88153549945e59ff0d08acc6e96
Nita <nitaeduarddd@gmail.com>
2022-12-02 23:56:32 +0200

    Add day2

---
+++ day2/app/Main.hs
@@ -0,0 +1,18 @@
+module Main (main) where
+
+import Lib
+import Text.Regex.TDFA
+import Data.Char (chr, ord)
+
+main :: IO ()
+main = do
+    text <- readFile "day2.in"
+    let input = map tail (text =~ "([ABC]) ([XYZ])+")::[[String]]
+    let ints = [map ord (concat x) | x <- input]
+    let norm = [[a-65, b-88] | [a,b] <- ints]
+    let enums = [[toEnum y::Sign | y <- x] | x <- norm]
+    let scores = map findScore enums
+    print(sum scores) -- First Part
+    print(enums)
+    let rigged = [[a, changeSign a b] | [a, b] <- enums]
+    print(sum $ map findScore rigged) -- Second part
---
+++ day2/src/Lib.hs
@@ -0,0 +1,44 @@
+module Lib
+    ( 
+    findScore,
+    changeSign,
+    Sign
+    ) where
+
+import Data.Char (chr, ord)
+
+instance Num Char where
+    c1 + c2 = chr (ord c1 + ord c2)
+    c1 - c2 = chr (ord c1 - ord c2)
+    fromInteger = chr . fromInteger
+
+data Sign = Rock | Paper | Scissor deriving (Enum, Eq, Show)
+
+tuplify2 :: [a] -> (a,a)
+tuplify2 [x,y] = (x,y)
+
+compareSign :: Sign -> Sign -> Ordering
+compareSign s1 s2 
+    | s1 == s2 = EQ
+    | s1 == Rock && s2 == Scissor = GT
+    | s1 == Rock && s2 == Paper = LT
+    | s1 == Scissor && s2 == Paper = GT
+    | s1 == Scissor && s2 == Rock = LT
+    | s1 == Paper && s2 == Rock = GT
+    | s1 == Paper && s2 == Scissor = LT
+
+changeSign :: Sign -> Sign -> Sign
+changeSign s1 s2
+    | s2 == Paper = s1
+    | s1 == Rock = if s2 == Rock then Scissor else Paper
+    | s1 == Paper = if s2 == Rock then Rock else Scissor 
+    | s1 == Scissor = if s2 == Rock then Paper else Rock
+
+findScore :: [Sign] -> Int
+findScore s = do
+    let (s1, s2) = tuplify2 s
+    case compareSign s1 s2 of   
+        GT -> 0 + fromEnum s2 + 1
+        EQ -> 3 + fromEnum s2 + 1
+        LT -> 6 + fromEnum s2 + 1
+                                  
