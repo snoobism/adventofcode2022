069e7be761a54042a524d6acde95d7964177f100
Nita <nitaeduarddd@gmail.com>
2022-12-09 16:55:28 +0200

    Add day9

---
+++ day9/app/Main.hs
@@ -0,0 +1,14 @@
+module Main (main) where
+
+import Lib
+import qualified Data.Set as Set 
+
+main :: IO ()
+main = do
+    text <- readFile "day9.in"
+    let directions = [(x, (read::String->Int) y) | [x, y] <- map words (lines text)]
+    let headTrail = reverse $ headDirections directions (0, 0) []
+    let tailTrail = reverse $ tailDirections headTrail (0, 0) (0, 0) []
+    print $ length $ Set.fromList tailTrail -- First part
+    let nTrail = applyN headTrail 9 
+    print $ length $ Set.fromList nTrail -- Second part
---
+++ day9/src/Lib.hs
@@ -0,0 +1,34 @@
+module Lib
+    ( move, 
+    headDirections,
+    tailDirections, 
+    applyN
+    ) where
+
+move :: (String, Int) -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+move (_, 0) _ trail = trail
+move ("R", num) (x,y) trail = (move ("R", num - 1) (x+1, y) ((x+1,y):trail)) 
+move ("L", num) (x,y) trail = (move ("L", num - 1) (x-1, y) ((x-1,y):trail))
+move ("U", num) (x,y) trail = (move ("U", num - 1) (x, y+1) ((x,y+1):trail)) 
+move ("D", num) (x,y) trail = (move ("D", num - 1) (x, y-1) ((x,y-1):trail)) 
+
+headDirections :: [(String, Int)] -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+headDirections [] _ trail = trail
+headDirections (m:ms) pos trail = do
+    let moveTrail@(x:xs) = move m pos trail
+    headDirections ms x (move m pos trail)
+
+tailDirections :: [(Int, Int)] -> (Int, Int) -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+tailDirections [] old pos trail = (pos:trail) 
+tailDirections ((hx, hy):ms) (oldHx, oldHy) pos@(tx, ty) trail = do
+    let (nextX, nextY) = case ((abs(hx-tx) <= 1), (abs(hy-ty) <= 1)) of
+            (True, True) -> (tx, ty)
+            (False, True) -> ((hx+tx) `div` 2, hy)
+            (True, False) -> (hx, (hy+ty) `div` 2)
+            (False, False) -> ((hx+tx) `div` 2, (hy+ty) `div` 2)
+    tailDirections ms (hx, hy) (nextX, nextY) (pos:trail)
+
+applyN :: [(Int, Int)] -> Int -> [(Int, Int)]
+applyN headTrail 0 = headTrail
+applyN headTrail n = applyN (reverse $ tailDirections headTrail (0, 0) (0, 0) []) (n-1)
+    
